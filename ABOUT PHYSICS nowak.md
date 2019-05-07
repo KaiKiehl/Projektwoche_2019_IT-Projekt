d5b673d9b691434ae05f5d2c03cab8dbbaeeeb20
WinzuDarkpaw <50320269+WinzuDarkpaw@users.noreply.github.com>
2019-05-07 13:59:27 +0200

    Add files via upload

---
+++ ABOUT PHYSICS nowak.md
@@ -0,0 +1,30 @@
+LIST OF MODIFIED FILES
+
+\Projektwoche_2019_It-Projekt\MyProject\Source\MyProject\MyPawnMovementComponent.cpp
+		--	FOR
+		GRAVITY, TOUCH AND MOVEMENT-WHEN TOUCHED
+
+\Projektwoche_2019_It-Projekt\MyProject\Source\MyProject\MyPawnMovementComponent.h
+		--	FOR
+		DEPENDENCY FOR MyPawnMovementComponent.cpp
+		DO NOT CHANGE THE PATHS OF THE FILES
+INFORMATION
+--	DO NOT CHANGE DATA HIERARCHY
+
+07/05/2019
+ADDED MyPawnMovementComponent.cpp
+--	ADDED Gravity
+--	ADDED Touch and Collision
+--	ADDED Movement when touched
+ADDED MyPawnMovementComponent.h
+--	ADDED Dependency
+ADDED MovingPawn.cpp
+--	ADDED Movement to Player
+ADDED MovingPawn.h
+--	DEPENDENCY
+
+
+
+VERY IMPORTANT
+
+DATA HAVE TO BE INPLEMENTED FIRST!!!!!!!
---
+++ MovingPawn.cpp
@@ -0,0 +1,103 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPawn.h"
+#include "Camera/CameraComponent.h"
+#include "MyPawnMovementComponent.h"
+#include "Components/InputComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SphereComponent.h"
+#include "ConstructorHelpers.h"
+#include "GameFramework/SpringArmComponent.h"
+
+// Sets default values
+AMovingPawn::AMovingPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	USphereComponent* SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("RootComponent"));
+	RootComponent = SphereComponent;
+	SphereComponent->InitSphereRadius(40.f);
+	SphereComponent->SetCollisionProfileName(TEXT("Pawn"));
+
+	UStaticMeshComponent* SphereVisual = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Visual Representation"));
+	SphereVisual->SetupAttachment(RootComponent);
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SphereVisualAsset(TEXT("/Game/StarterContent/Shapes/Shapes_Sphere.Shape_Sphere"));
+
+	if (SphereVisualAsset.Succeeded())
+	{
+		SphereVisual->SetStaticMesh(SphereVisualAsset.Object);
+		SphereVisual->SetRelativeLocation(FVector(0.0f, 0.0f, -40.0f));
+		SphereVisual->SetWorldScale3D(FVector(0.8f));
+	}
+
+	USpringArmComponent* SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraAttachement"));
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->RelativeRotation = FRotator(-45.f, 0.f, 0.f);
+	SpringArm->TargetArmLength = 400.0f;
+	SpringArm->bEnableCameraLag = true;
+	SpringArm->CameraLagSpeed = 3.0f;
+
+	UCameraComponent* Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Our Camera"));
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+	OurMovementComponent = CreateDefaultSubobject<UPawnMovementComponent>(TEXT("Custom Move Component"));
+	OurMovementComponent->UpdatedComponent = RootComponent;
+}
+
+// Called when the game starts or when spawned
+void AMovingPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMovingPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMovingPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	InputComponent->BindAxis("MoveForward", this, &AMovingPawn::MoveForward);
+	InputComponent->BindAxis("MoveRight", this, &AMovingPawn::MoveRight);
+	InputComponent->BindAxis("Turn", this, &AMovingPawn::Turn);
+
+}
+
+
+UPawnMovementComponent* AMovingPawn::GetMovementComponent() const
+{
+	return OurMovementComponent;
+}
+
+void AMovingPawn::MoveForward(float AxisValue)
+{
+	if (OurMovementComponent && (OurMovementComponent->UpdatedComponent == RootComponent))
+	{
+		OurMovementComponent->AddInputVector(GetActorForwardVector()*AxisValue);
+	}
+}
+
+void AMovingPawn::MoveRight(float AxisValue)
+{
+	if (OurMovementComponent && (OurMovementComponent->UpdatedComponent == RootComponent))
+	{
+		OurMovementComponent->AddInputVector(GetActorRightVector()*AxisValue);
+	}
+}
+
+void AMovingPawn::Turn(float AxisValue)
+{
+	FRotator NewRotation = GetActorRotation();
+	NewRotation.Yaw += AxisValue;
+	SetActorRotation(NewRotation);
+}
