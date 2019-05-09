0e399815e3a37fd24edda023f7b10b2a56a89c53
WinzuDarkpaw <50320269+WinzuDarkpaw@users.noreply.github.com>
2019-05-09 13:57:40 +0200

    Update MyPawnMovementComponent.cpp

--- MyPawnMovementComponent.cpp
+++ MyPawnMovementComponent.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+/*
 #include "MyPawnMovementComponent.h"
 void UMyPawnMovementComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
@@ -24,3 +24,4 @@ void UMyPawnMovementComponent::TickComponent(float DeltaTime, enum ELevelTick Ti
 		}
 	}
 }
+*/
